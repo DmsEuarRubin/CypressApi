2f5c61c2000223c5008c41295ecde306eaa2585c
Pickle RIIIICK <125954092+DmsEuarRubin@users.noreply.github.com>
2024-01-27 15:42:09 +0400

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,39 @@
-Cypress.Commands.add('Myexpecting', (value, valueTwo) => {
-    expect(value).to.eq(valueTwo);
+Cypress.Commands.add('creation', (name, gender, email, status) => {
+    cy.request({
+        url: "/",
+        method: 'POST',
+        body: {
+        "name": name, 
+        "gender": gender, 
+        "email": email, 
+        "status": status
+        },
+        headers: {
+            "Authorization": Cypress.env("token")
+        }
+    })
+})
+Cypress.Commands.add('getting', (randomDate, response, ID) => {
+    ID = response.body.id;
+     expect(response.body.name).to.eq(`VAR${randomDate}VAR`);
+     expect(response.body.email).to.eq(`test${randomDate}@fakemail.am`);
+     cy.request({
+         url: `/${response.body.id}`,
+         method: 'GET',
+        headers: {
+            "Authorization": Cypress.env("token")
+        }
+    })
+})
+Cypress.Commands.add('deleteing', (randomDate, response, ID) => {
+    expect(response.body.id, ID);
+            expect(response.body.name).to.eq(`VAR${randomDate}VAR`);
+            expect(response.body.email).to.eq(`test${randomDate}@fakemail.am`);
+            cy.request({
+                url: `/${response.body.id}`,
+                method: 'DELETE',
+                headers: {
+                    "Authorization": Cypress.env("token")
+                }
+            })
 })

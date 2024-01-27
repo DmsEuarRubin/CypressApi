d2d9ae5be50ba1dcd0748ed029a45a86dd38dbce
Pickle RIIIICK <125954092+DmsEuarRubin@users.noreply.github.com>
2024-01-27 15:41:20 +0400

    Update CypressApiTesting.js

--- cypress/e2e/2-advanced-examples/CypressApiTesting.js
+++ cypress/e2e/2-advanced-examples/CypressApiTesting.js
@@ -1,49 +1,14 @@
 describe('Gorest : API testing', () => {
-    const randomDate = Date.now();
-    it('Gorest | POST', () => {
-    cy.fixture('UserInfo').then((data) => {
-        let UserId;
-        data.name = `VAR${randomDate}VAR`;
-        data.email = `test${randomDate}@fakemail.am`;
-        cy.log(data)
-        cy.request({
-            url: "/",
-            method: 'POST',
-            body: {
-            "name": data.name, 
-            "gender": data.gender, 
-            "email": data.email, 
-            "status": data.status
-            },
-            headers: {
-                "Authorization": Cypress.env("token")
-            }
-        }).then(response => {
-            UserId = response.body.id;
-            cy.Myexpecting(response.body.name, `VAR${randomDate}VAR`);
-            cy.Myexpecting(response.body.email, `test${randomDate}@fakemail.am`);
-            cy.request({
-                url: `/${response.body.id}`,
-                method: 'GET',
-                headers: {
-                    "Authorization": Cypress.env("token")
-                }
-            })
-
-        }).then(response => {
-            cy.Myexpecting(response.body.id, UserId);
-            cy.Myexpecting(response.body.name, `VAR${randomDate}VAR`);
-            cy.Myexpecting(response.body.email, `test${randomDate}@fakemail.am`);
-            cy.request({
-                url: `/${response.body.id}`,
-                method: 'DELETE',
-                headers: {
-                    "Authorization": Cypress.env("token")
-                }
-            })
-        }).then(response => {
-            expect(response.status).to.eq(204);
+    it('Gorest | API', () => {
+        cy.fixture('UserInfo').then((data) => {
+            let UserId;
+            const randomDate = Date.now();
+            data.name = `VAR${randomDate}VAR`;
+            data.email = `test${randomDate}@fakemail.am`;
+            cy.creation(data.name,data.gender,data.email,data.status).then(response => {
+            cy.getting(randomDate, response, UserId)}).then(response => {
+            cy.deleteing(randomDate, response, UserId)}).then(response => {
+            expect(response.status).to.eq(204)})
         })
-    })
     })
 });
